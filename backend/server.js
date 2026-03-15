26c04583cdbf79b54ba370995981416486b2e2eb
Dharmik <dharmiksuthar2405@gmail.com>
2026-03-15 14:24:50 +0530

    Fix CORS for Vercel deployment, add start script

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,22 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:3000',
+  process.env.FRONTEND_URL,
+].filter(Boolean);
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.some(o => origin.startsWith(o))) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+}));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
